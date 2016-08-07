87a6df594f4284e44e8f5ce604d4e84c79a8fe98
Photkeerati <pojkeerati@gmail.com>
2016-08-08 00:06:47 +0700

    Binding datasource with Pokemon model

--- 16_Pokedex3/ViewController.swift
+++ 16_Pokedex3/ViewController.swift
@@ -8,19 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout  {
 
+    @IBOutlet weak var collectionView:UICollectionView!
+    var pokemon = [Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("test")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+       
+        parsePokemonCSV()
+        
+    }
+    
+    func parsePokemonCSV() {
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")
+        do {
+           let csv = try CSV.init(contentsOfURL: path!)
+            let rows = csv.rows
+            for row in rows {
+                let pokeID = Int(row["id"]!)!
+                let name = row["identifier"]!
+                pokemon.append(Pokemon(name: name, pokedexId: pokeID))
+            }
+            
+        } catch {
+            
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pokemon.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            cell.configureCell(pokemon: pokemon[indexPath.row])
+            return cell
+        }
+        
+         return UICollectionViewCell()
     }
 
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 90, height: 100)
+    }
 
 }
 
